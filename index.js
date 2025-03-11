89e5e2473eba45f94a59d8d3cd60c6f232df515b
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-03-11 19:01:55 +0600

    Configure MongoDB Connection

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bjzga.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 
 app.get("/", (req, res) => {
   res.send("All Campaign");
